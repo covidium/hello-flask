3b6342fff4ee75566d1006f671c1593a23dba6f7
Ovidiu Calbajos <o.calbajos@netmatch.ro>
2018-09-20 09:58:32 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+
+import os
+from flask import Flask
+app = Flask(__name__)
+
+host_ip = '0.0.0.0'
+
+@app.route("/")
+def hello():
+    return "Hello from Python!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+app.run(host=host_ip, port=port)
